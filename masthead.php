87e364237213f8bb24bc5cfe917ec117344d2e6b
George Stephanis <george@stephanis.info>
2013-08-24 09:38:53 -0500

    Extracting context to masthead.php

---
+++ masthead.php
@@ -0,0 +1,8 @@
+
+<header class="masthead">
+	<hgroup class="wrap">
+		<img src="http://placekitten.com/400/200" width="400" height="200" alt="Placekitten!" />
+		<h1>Whether you&rsquo;re a budding developer or UX pro, we&rsquo;re always looking for people to join up and help make WordPress even greater.</h1>
+		<h5>This new site is a work in progress, and will be your source for learning how to get involved with WordPress.  It will house blogs for each contributor group, general news, and upcoming events.</h5>
+	</hgroup>
+</header>
