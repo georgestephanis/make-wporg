0029700926db3dc44a48899fc63231066d4d7418
George Stephanis <george@stephanis.info>
2013-09-09 21:18:59 -0400

    In the masthead, loop through the post/page/whatever.

--- masthead.php
+++ masthead.php
@@ -1,8 +1,13 @@
 
 <header class="masthead">
-	<hgroup class="wrap">
-		<img src="http://placekitten.com/400/200" width="400" height="200" alt="Placekitten!" />
-		<h1>Whether you&rsquo;re a budding developer or UX pro, we&rsquo;re always looking for people to join up and help make WordPress even greater.</h1>
-		<h5>This new site is a work in progress, and will be your source for learning how to get involved with WordPress.  It will house blogs for each contributor group, general news, and upcoming events.</h5>
-	</hgroup>
+	<?php while ( have_posts() ) : the_post(); ?>
+		<hgroup <?php post_class( 'wrap' ); ?>>
+			<?php
+				if ( have_post_thumbnail() ) {
+					the_post_thumbnail( array( 400, 200 ) );
+				}
+				the_content();
+			?>
+		</hgroup>
+	<?php endwhile; ?>
 </header>
